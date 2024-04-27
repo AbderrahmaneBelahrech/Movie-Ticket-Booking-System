9a5786a37f9dbb815be0f4ff8ee3d9b4049032e4
oussama-f <121732561+oussama-f@users.noreply.github.com>
2024-04-27 15:57:58 +0100

    Update App.js

--- cinema-website frontend/src/App.js
+++ cinema-website frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ListeFilms from "./Pages/ListeFilms";
+import ListeFilms from "./pages/ListeFilms";
 import "./App.css";
 
 function App() {
